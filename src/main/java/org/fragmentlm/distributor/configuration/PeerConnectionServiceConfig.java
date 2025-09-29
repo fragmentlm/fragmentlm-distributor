aff5b67e4a38daceb785807296b9ff1ec5373146
nikgub_ <danger8217.nik@gmail.com>
2025-09-29 16:36:12 +0300

    Extend DI

--- src/main/java/org/fragmentlm/distributor/configuration/PeerConnectionServiceConfig.java
+++ src/main/java/org/fragmentlm/distributor/configuration/PeerConnectionServiceConfig.java
@@ -1,16 +1,33 @@
 package org.fragmentlm.distributor.configuration;
 
+import jakarta.validation.constraints.NotNull;
+import org.fragmentlm.distributor.dto.ProcessedFragments;
 import org.fragmentlm.distributor.service.FragmentFetcherService;
 import org.fragmentlm.distributor.service.IPeerConnectionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class PeerConnectionServiceConfig
 {
     @Bean
-    public IPeerConnectionService service()
+    public IPeerConnectionService service (@NotNull ProcessedFragments replyObject, @NotNull RestTemplate restTemplate)
     {
-        return new FragmentFetcherService();
+        return new FragmentFetcherService(replyObject, restTemplate);
+    }
+
+    @Bean
+    public @NotNull RestTemplate restTemplate ()
+    {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public @NotNull ProcessedFragments reply ()
+    {
+        return new ProcessedFragments(new ConcurrentHashMap<>());
     }
 }
